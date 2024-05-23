64cb4765d82790decce0ebd4aebad1e977757a2c
pairnon <janpairnon@gmail.com>
2024-05-23 08:48:28 -0700

    Update build config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,11 +9,18 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+
+    id("xyz.jpenilla.run-paper") version "2.2.0"
 }
 
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
+
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
@@ -22,14 +29,25 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.1-jre")
+
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
 }
 
 application {
     // Define the main class for the application.
-    mainClass.set("teamplugin.App")
+    mainClass.set("io.papermc.teamplugin.Main")
 }
 
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+}
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("1.20.2")
+    }
 }
