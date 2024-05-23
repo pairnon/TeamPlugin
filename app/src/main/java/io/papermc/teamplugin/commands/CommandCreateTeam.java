48a068caa25af1e09547924287807dae3c77b4a7
pairnon <janpairnon@gmail.com>
2024-05-23 09:19:01 -0700

    Add arg to createteam

--- app/src/main/java/io/papermc/teamplugin/commands/CommandCreateTeam.java
+++ app/src/main/java/io/papermc/teamplugin/commands/CommandCreateTeam.java
@@ -3,11 +3,17 @@ package io.papermc.teamplugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandCreateTeam implements CommandExecutor {
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        Player player = (Player) sender;
+
+        if (args.length == 0) { return false; }
+
 
 
         return true;
