b21a76c6bf1ff84eeb1877505cc2793cf2e8931b
pairnon <janpairnon@gmail.com>
2024-05-23 08:55:28 -0700

    Add Main class

---
+++ app/src/main/java/io/papermc/teamplugin/Main.java
@@ -0,0 +1,15 @@
+package io.papermc.teamplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+
+    }
+}
