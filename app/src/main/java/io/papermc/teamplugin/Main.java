7658f49f20571820d385740d9fafc282f6fa1a1f
pairnon <janpairnon@gmail.com>
2024-05-23 09:09:23 -0700

    Add createteam command

--- app/src/main/java/io/papermc/teamplugin/Main.java
+++ app/src/main/java/io/papermc/teamplugin/Main.java
@@ -4,6 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.papermc.teamplugin.commands.CommandCreateTeam;
+
 public class Main extends JavaPlugin implements Listener {
 
     @Override
@@ -11,5 +13,6 @@ public class Main extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
 
 
+        this.getCommand("createteam").setExecutor(new CommandCreateTeam());
     }
 }
