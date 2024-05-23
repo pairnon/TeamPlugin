c8e90fece8a4c8df915db86febd8e27b457733d1
pairnon <janpairnon@gmail.com>
2024-05-23 09:14:27 -0700

    Add Team class

---
+++ app/src/main/java/io/papermc/teamplugin/classes/Team.java
@@ -0,0 +1,19 @@
+package io.papermc.teamplugin.classes;
+
+public class Team {
+    
+    private String teamName;
+
+    public Team(String name) {
+        teamName = name;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String name) {
+        teamName = name;
+    }
+
+}
